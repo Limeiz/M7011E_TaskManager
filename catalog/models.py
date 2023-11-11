113a35df04aad8c8d4b0925f8ac0221757c34f57
Ellen Åsman <ellen.c.asman@gmail.com>
2023-11-11 15:02:40 +0100

    First commit, added model

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=20, help_text='Enter name')
+
+    class Meta:
+        ordering = ['-name']
+
+    def __str__(self):
+        return self.self.name
