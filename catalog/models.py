3ca84c7fa08a659c0004f9b34beee8eee16c7e0e
sanlju <sanlju-9@student.ltu.se>
2023-11-14 17:55:48 +0100

    Many-to-many relationship

--- catalog/models.py
+++ catalog/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.urls import reverse
 
-
 class User(models.Model):
     __tablename__ = 'User'
 
@@ -12,14 +11,14 @@ class User(models.Model):
     )
 
     user_id = models.AutoField(primary_key=True)
-    username = models.CharField(max_length=200)
+    username = models.CharField(max_length=200, unique=True)
     password = models.CharField(max_length=200)
-    email = models.EmailField()
+    email = models.EmailField(unique=True)
     user_type = models.CharField(max_length=1, choices=USER_TYPES, default='u')
     is_logged_in = models.BooleanField(default=False)
 
     def __str__(self):
-        return self.username
+        return f"{self.username}"
 
     def get_absolute_url(self):
         return reverse('user-detail', args=[str(self.id)])
@@ -41,8 +40,8 @@ class Task(models.Model):
     )
 
     task_id = models.AutoField(primary_key=True)
-    task_name = models.CharField(max_length=200)
-    note = models.TextField(blank=True)
+    task_name = models.CharField(max_length=200, unique=True)
+    description = models.TextField(blank=True)
     file = models.FileField(blank=True)
     priority = models.CharField(max_length=1, choices=TASK_PRIORITY, blank=True, help_text='Task priority')
     deadline = models.DateTimeField(null=True, blank=True)
@@ -52,7 +51,7 @@ class Task(models.Model):
         ordering = ['priority']
 
     def __str__(self):
-        return self.task_name
+        return f"{self.task_name}"
 
     def get_absolute_url(self):
         return reverse('task-detail', args=[str(self.id)])
@@ -62,26 +61,26 @@ class Reminder(models.Model):
     __tablename__ = 'Reminder'
     reminder_id = models.AutoField(primary_key=True)
     date = models.DateTimeField()
-    task_id = models.ForeignKey(Task, on_delete=models.RESTRICT)
-    user_id = models.ForeignKey(User, on_delete=models.RESTRICT)
+    task_name = models.ForeignKey(Task, to_field='task_name', on_delete=models.RESTRICT)
+    user_name = models.ForeignKey(User, to_field='username', on_delete=models.RESTRICT)
 
     class Meta:
         ordering = ["date"]
 
     def __str__(self):
-        return self.reminder_id
+        return f"{self.reminder_id}"
 
     def get_absolute_url(self):
         return reverse('reminder-detail', args=[str(self.id)])
 
-
 class List(models.Model):
     __tablename__ = 'List'
     list_id = models.AutoField(primary_key=True)
-    user_id = models.ForeignKey(User, on_delete=models.RESTRICT)
+    list_name = models.CharField(max_length=200, unique=True)
+    assigned_users = models.ManyToManyField(User)
 
     def __str__(self):
-        return self.list_id
+        return f"{self.list_name}"
 
     def get_absolute_url(self):
         return reverse('list-detail', args=[str(self.id)])
@@ -91,52 +90,11 @@ class ListEntry(models.Model):
     """Model representing a list entry."""
     __tablename__ = 'ListEntry'
     listentry_id = models.AutoField(primary_key=True)
-    task_id = models.ForeignKey(Task, on_delete=models.RESTRICT)
-    list_id = models.ForeignKey(List, on_delete=models.RESTRICT)
+    assigned_tasks = models.ManyToManyField(Task)
+    list_name = models.ForeignKey(List, to_field='list_name', on_delete=models.RESTRICT)
 
     def __str__(self):
-        return self.listentry_id
+        return f"{self.listentry_id}"
 
     def get_absolute_url(self):
         return reverse('listentry-detail', args=[str(self.id)])
-
-
-class Group(models.Model):
-    __tablename__ = 'Group'
-    group_id = models.AutoField(primary_key=True)
-    group_name = models.CharField(max_length=200)
-
-    class Meta:
-        ordering = ["group_name"]
-
-    def __str__(self):
-        return self.group_id
-
-    def get_absolute_url(self):
-        return reverse('group-detail', args=[str(self.id)])
-
-
-class GroupEntry(models.Model):
-    __tablename__ = 'GroupEntry'
-    groupentry_id = models.AutoField(primary_key=True)
-    user_id = models.ForeignKey(User, on_delete=models.RESTRICT)
-    group_id = models.ForeignKey(Group, on_delete=models.RESTRICT)
-
-    def __str__(self):
-        return self.groupentry_id
-
-    def get_absolute_url(self):
-        return reverse('groupentry-detail', args=[str(self.id)])
-
-
-class SharedList(models.Model):
-    __tablename__ = 'SharedList'
-    sharedlist_id = models.AutoField(primary_key=True)
-    group_id = models.ForeignKey(Group, on_delete=models.RESTRICT)
-    list_id = models.ForeignKey(List, on_delete=models.RESTRICT)
-
-    def __str__(self):
-        return self.sharedlist_id
-
-    def get_absolute_url(self):
-        return reverse('sharedList-detail', args=[str(self.id)])
