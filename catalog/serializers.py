fb15fc5a68380c28b9e1344c9e02f1f3c8ba42c5
sanlju <sanlju-9@student.ltu.se>
2024-01-07 15:11:55 +0100

    restructure

--- catalog/serializers.py
+++ catalog/serializers.py
@@ -2,48 +2,54 @@ from rest_framework import serializers
 from .models import User, Task, List, Reminder, Achievement
 
 
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ['username', 'email', 'password']
+#
+# class UserSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = User
+#         fields = ['username', 'email', 'password']
 
 
 class TaskSerializer(serializers.ModelSerializer):
-    task_slug = serializers.SlugRelatedField(
-        read_only=True,
-        source='task',
-        slug_field='slug'
-    )
-
-    task = serializers.HyperlinkedRelatedField(
-        view_name='task:task_admin_details',
-        read_only=True,
-        lookup_field='slug'
-    )
+    # task_slug = serializers.SlugRelatedField(
+    #     read_only=True,
+    #     source='task',
+    #     slug_field='slug'
+    # )
+    #
+    # task = serializers.HyperlinkedRelatedField(
+    #     view_name='task:task_admin_details',
+    #     read_only=True,
+    #     lookup_field='slug'
+    # )
+    #
+    assignee = serializers.CharField(source='assignee.username') #read_only=True
+    status = serializers.ChoiceField(choices=Task.TASK_STATUS,
+                                     source='get_status_display')
+    priority = serializers.ChoiceField(choices=Task.TASK_PRIORITY,
+                                       source='get_priority_display')
 
     class Meta:
         model = Task
         fields = ['task_name', 'description', 'file', 'priority', 'deadline',
-                  'status', 'slug', 'task_slug', 'task', 'assignee']
+                  'status', 'assignee', 'slug']
 
 
 class ReminderSerializer(serializers.ModelSerializer):
-    reminder_slug = serializers.SlugRelatedField(
-        read_only=True,
-        source='reminder',
-        slug_field='slug'
-    )
-
-    reminder = serializers.HyperlinkedRelatedField(
-        view_name='reminder:reminder_admin_details',
-        read_only=True,
-        lookup_field='slug'
-    )
+    # reminder_slug = serializers.SlugRelatedField(
+    #     read_only=True,
+    #     source='reminder',
+    #     slug_field='slug'
+    # )
+    #
+    # reminder = serializers.HyperlinkedRelatedField(
+    #     view_name='reminder:reminder_admin_details',
+    #     read_only=True,
+    #     lookup_field='slug'
+    # )
 
     class Meta:
         model = Reminder
-        fields = ['task_id', 'username', 'date', 'slug', 'reminder_slug',
-                  'reminder']
+        fields = ['task_id', 'username', 'date', 'slug']
 
 
 class UserUsernameSerializer(serializers.ModelSerializer):
@@ -56,21 +62,21 @@ class ListSerializer(serializers.ModelSerializer):
     assigned_users = UserUsernameSerializer(many=True)
     assigned_tasks = TaskSerializer(many=True)
 
-    list_slug = serializers.SlugRelatedField(
-        read_only=True,
-        source='list',
-        slug_field='slug'
-    )
-
-    list = serializers.HyperlinkedRelatedField(
-        view_name='list:list_admin_details',
-        read_only=True,
-        lookup_field='slug'
-    )
+    # list_slug = serializers.SlugRelatedField(
+    #     read_only=True,
+    #     source='list',
+    #     slug_field='slug'
+    # )
+    #
+    # list = serializers.HyperlinkedRelatedField(
+    #     view_name='list:list_admin_details',
+    #     read_only=True,
+    #     lookup_field='slug'
+    # )
 
     class Meta:
         model = List
-        fields = ('list_name', 'slug', 'list_slug', 'list', 'assigned_users',
+        fields = ('list_name', 'slug', 'assigned_users',
                   'assigned_tasks')
 
 
